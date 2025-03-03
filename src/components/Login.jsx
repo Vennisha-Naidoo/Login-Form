762f5905bb28c66e232a8c5955ebff054c0bd0fa
Vennisha Naidoo <89158888+Vennisha-Naidoo@users.noreply.github.com>
2025-03-04 01:39:33 +0200

    Getting User Input using useRef

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,22 +1,17 @@
-import { useState } from "react";
+import { useRef } from "react";
 
 export default function Login() {
 
-  const [enteredCredentials, setEnteredCredentials] = useState({
-    email: '',
-    password: ''
-  });
+  const email = useRef();
+  const password = useRef();
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log("Values: ", enteredCredentials);
-  }
 
-  function handleInputChange(identifer, value) {
-    setEnteredCredentials(prevCreds => ({
-      ...prevCreds,
-      [identifer]: value
-    }))
+    const enteredEmail = email.current.value;
+    const enteredPassword= password.current.value;
+
+    console.log(enteredEmail, enteredPassword);
   }
 
   return (
@@ -26,12 +21,12 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" onChange={ (event) => handleInputChange('email', event.target.value) } value={ enteredCredentials.email } />
+          <input id="email" type="email" name="email" ref={ email } />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" onChange={ (event) => handleInputChange('password', event.target.value) } value={ enteredCredentials.password } />
+          <input id="password" type="password" name="password" ref={ password } />
         </div>
       </div>
 
