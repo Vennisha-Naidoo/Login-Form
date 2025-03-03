3145ddc9c875fc69b529acac1830661f11f264c1
Vennisha Naidoo <89158888+Vennisha-Naidoo@users.noreply.github.com>
2025-03-04 01:14:04 +0200

    Getting User Input and Using Generic Handlers

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,21 +2,21 @@ import { useState } from "react";
 
 export default function Login() {
 
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [enteredPassword, setEnteredPassword] = useState('');
+  const [enteredCredentials, setEnteredCredentials] = useState({
+    email: '',
+    password: ''
+  });
 
   function handleSubmit(event) {
     event.preventDefault();
-    // console.log("User email: ", enteredEmail);
-    // console.log("User password: ", enteredPassword);
+    console.log("Values: ", enteredCredentials);
   }
 
-  function handleEmailChange(event) {
-    setEnteredEmail(event.target.value);
-  }
-
-  function handlePasswordChange(event) {
-    setEnteredPassword(event.target.value);
+  function handleInputChange(identifer, value) {
+    setEnteredCredentials(prevCreds => ({
+      ...prevCreds,
+      [identifer]: value
+    }))
   }
 
   return (
@@ -26,12 +26,12 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" onChange={ handleEmailChange } value={ enteredEmail } />
+          <input id="email" type="email" name="email" onChange={ (event) => handleInputChange('email', event.target.value) } value={ enteredCredentials.email } />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" onChange={ handlePasswordChange } value={ enteredPassword } />
+          <input id="password" type="password" name="password" onChange={ (event) => handleInputChange('password', event.target.value) } value={ enteredCredentials.password } />
         </div>
       </div>
 
