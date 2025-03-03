d596f39c3b60f4675b8e49f2e21c020f029ad614
Vennisha Naidoo <89158888+Vennisha-Naidoo@users.noreply.github.com>
2025-03-04 01:53:45 +0200

    Signup Form

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,6 @@ export default function Header() {
     <header>
       <img src={logoImg} alt="A form and a pencil" />
       <h1>Welcome!</h1>
-      <h5>Please Enter Login Credentials.</h5>
     </header>
   );
 }
